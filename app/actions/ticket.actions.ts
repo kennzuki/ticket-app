202e11a63b3abbb7ac5189877681b233f1e461b5
kennzuki <kingsyken@gmail.com>
2025-05-31 14:53:04 +0300

    prisma client global setup

--- app/actions/ticket.actions.ts
+++ app/actions/ticket.actions.ts
@@ -3,10 +3,19 @@ export async function createTicket(
   prevState: { sucess: boolean; messsage: string },
   formData: FormData
 ): Promise<{ success: boolean; message: string }> {
+
+ // Extracting form data
   const subject = formData.get('subject')?.toString() || ('' as string);
   const description = formData.get('description')?.toString() || ('' as string);
-  const priority = formData.get('priority')?.toString() || ('Low' as string);
-
+    const priority = formData.get('priority')?.toString() || ('Low' as string);
+    
+    //error in the form data
+    if (!subject || !description || !priority) {
+        return {
+            success: false,
+            message: 'Subject, description and priority are required',
+        }
+    }
   // Here you would typically send the data to your backend or API
   // For demonstration, we will just log it to the console
   console.log('Ticket Created:', { subject, description, priority });
