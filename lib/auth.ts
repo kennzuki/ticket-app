73cea302622ec165731d933176b22415ad73a8d3
kennzuki <kingsyken@gmail.com>
2025-06-02 16:08:59 +0300

    encrypt and sign in jwt token

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,21 @@
-import { SignJWT,jwtVerify } from "jose";
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+const secret = new TextEncoder().encode(process.env.AUTH_SECRET_KEY);
+const cookieName = "auth_token";
+
+//encrypt the payload and sign the JWT token
+export async const signAuthToken = async (payload: string) => { 
+    try {
+        const token = await new SignJWT(payload)
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("7d")
+            .sign(secret);
+        return token;
+    } catch (error) {
+        throw new Error("Error signing auth token");
+    }
+
+}
+
