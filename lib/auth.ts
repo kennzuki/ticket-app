698c7e455c17418de4fb41ab18b4717a36952eed
kennzuki <kingsyken@gmail.com>
2025-06-02 23:25:11 +0300

    setting auth cookie

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,32 +1,46 @@
-import { SignJWT, jwtVerify } from "jose";
-import { cookies } from "next/headers";
+import { SignJWT, jwtVerify } from 'jose';
+import { cookies } from 'next/headers';
 
 const secret = new TextEncoder().encode(process.env.AUTH_SECRET_KEY);
-const cookieName = "auth_token";
+const cookieName = 'auth_token';
 
 //encrypt the payload and sign the JWT token
-export async function signAuthToken (payload: string) { 
-    try {
-        const token = await new SignJWT(payload)
-        .setProtectedHeader({ alg: "HS256" })
-        .setIssuedAt()
-        .setExpirationTime("7d")
-            .sign(secret);
-        return token;
-    } catch (error) {
-        throw new Error("Error signing auth token");
-    }
-
+export async function signAuthToken(payload: string) {
+  try {
+    const token = await new SignJWT(payload)
+      .setProtectedHeader({ alg: 'HS256' })
+      .setIssuedAt()
+      .setExpirationTime('7d')
+      .sign(secret);
+    return token;
+  } catch (error) {
+    throw new Error('Error signing auth token');
+  }
 }
 
 //decrypt and verify token
-export async function verifyAuthToken<T> (token: string): Promise<T> {
-    try {
-        const { payload } = await jwtVerify(token, secret);
-        return payload as T;
-    } catch (error) {
-        error.message = "Invalid or expired token";
-        throw new Error("Error verifying auth token");
-    }
+export async function verifyAuthToken<T>(token: string): Promise<T> {
+  try {
+    const { payload } = await jwtVerify(token, secret);
+    return payload as T;
+  } catch (error) {
+    error.message = 'Invalid or expired token';
+    throw new Error('Error verifying auth token');
+  }
 }
 
+export async function setAuthCookie(token: string) {
+  try {
+    const cookieStore = await cookies();
+    cookieStore.set(cookieName, token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 7,
+    });
+  } catch (error) {
+    error.message
+    throw new Error('Error setting auth cookie');
+  }
+}
